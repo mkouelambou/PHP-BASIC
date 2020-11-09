455837f26577360843755591b3ab940ab64f0fd3
Mike <mike.kouelambou@student.rocva.nl>
2020-11-09 10:14:58 +0100

    save changes

---
+++ 3-Arrays/taak01/taak12.php
@@ -0,0 +1,9 @@
+<?php
+$myMovies = array("Spiderman", "Batman", "The Joker");
+$movie1 = "Spiderman";
+$movie2 = "Batman";
+$movie3 = "The Joker";
+echo $movie1  = "<li>Spiderman</li>";
+echo $movie2  = "<li>Batman</li>";
+echo $movie3  = "<li>The Joker</li>";
+?>
