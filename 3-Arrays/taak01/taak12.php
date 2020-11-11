67dfbc450c6a5de630fc6a11408193e598866cbd
Mike <mike.kouelambou@student.rocva.nl>
2020-11-11 11:10:16 +0100

    save changes

--- 3-Arrays/taak01/taak12.php
+++ 3-Arrays/taak01/taak12.php
@@ -1,9 +1,4 @@
 <?php
 $myMovies = array("Spiderman", "Batman", "The Joker");
-$movie1 = "Spiderman";
-$movie2 = "Batman";
-$movie3 = "The Joker";
-echo $movie1  = "<li>Spiderman</li>";
-echo $movie2  = "<li>Batman</li>";
-echo $movie3  = "<li>The Joker</li>";
+echo "<li>$myMovies[0]</li><li>$myMovies[1]</li><li>$myMovies[2]</li>";
 ?>
---
+++ 4-Arithmetics/taak02/taak15.php
@@ -0,0 +1,11 @@
+<?php
+$getal1 = 5;
+echo $getal1 += 10;
+echo "<br>";
+echo $getal1 -= 10;
+echo "<br>";
+echo $getal1 *= 10;
+echo "<br>";
+echo $getal1 /= 10;
+echo "<br>";
+?>
