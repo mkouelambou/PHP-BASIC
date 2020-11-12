cb68ab9d1b754e9e2f766aefa345cc1bee7ef1e9
Mike <mike.kouelambou@student.rocva.nl>
2020-11-12 15:05:39 +0100

    save changes

---
+++ 5-Conditional_Statements/taak06/index.php
@@ -0,0 +1,23 @@
+<?php
+$x = 75;
+$y = 25;
+
+if ($x == 75 && $y = 25) {
+    echo "beide waardes zijn waar". "<br>";
+}else {
+    echo "beide waardes zijn niet waar". "<br>";
+}
+
+$x = 75;
+$y = 25;
+
+if ($x == 75 || $y = 20) {
+    echo "een of beide waardes zijn waar". "<br>";
+}else {
+    echo "geen van beide waardes is waar". "<br>";
+}
+
+$x = 75;
+$y = 25;
+
+if ($x )
