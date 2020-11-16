8054dae0c45c4b87fa7c4e97ca10ad9595089b3a
Mike <mike.kouelambou@student.rocva.nl>
2020-11-16 12:26:35 +0100

    save

---
+++ 7-Include-Require/taak01/Includes/menu.php
@@ -0,0 +1,8 @@
+<?php
+$menu1 = "Speelgoed";
+$menu2 = "Elektronica";
+$menu3 = "Kleding";
+echo $menu1  = "<li>Speelgoed</li>";
+echo $menu2  = "<li>Elektronica</li>";
+echo $menu3  = "<li>Kleding</li>";
+?>
